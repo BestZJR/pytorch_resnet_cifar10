bc0c3bef492b4e6bba3e107a467394215dd46e21
BestZJR <140713400+BestZJR@users.noreply.github.com>
2023-08-05 20:24:16 +0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -177,7 +177,44 @@ def train(train_loader, model, criterion, optimizer, epoch, writer):
 
         # ...
         # 其他代码不变
+        # measure data loading time
+        data_time.update(time.time() - end)
 
+        target = target.cuda()
+        input_var = input.cuda()
+        target_var = target
+        if args.half:
+            input_var = input_var.half()
+
+        # compute output
+        output = model(input_var)
+        loss = criterion(output, target_var)
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        output = output.float()
+        loss = loss.float()
+        # measure accuracy and record loss
+        prec1 = accuracy(output.data, target)[0]
+        losses.update(loss.item(), input.size(0))
+        top1.update(prec1.item(), input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+        if i % args.print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
+                      epoch, i, len(train_loader), batch_time=batch_time,
+                      data_time=data_time, loss=losses, top1=top1))
+    
+        
         # Add data to TensorBoard
         writer.add_scalar('Train/Loss', losses.avg, epoch * len(train_loader) + i)
         writer.add_scalar('Train/Accuracy', top1.avg, epoch * len(train_loader) + i)
@@ -188,11 +225,51 @@ def train(train_loader, model, criterion, optimizer, epoch, writer):
 def validate(val_loader, model, criterion):
     # ...
     # 其他代码不变
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
 
-    for i, (input, target) in enumerate(val_loader):
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    with torch.no_grad():
+        for i, (input, target) in enumerate(val_loader):
         # ...
         # 其他代码不变
+            target = target.cuda()
+            input_var = input.cuda()
+            target_var = target.cuda()
 
+            if args.half:
+                input_var = input_var.half()
+
+
+            output = model(input_var)
+            loss = criterion(output, target_var)
+
+            output = output.float()
+            loss = loss.float()
+
+            # measure accuracy and record loss
+            prec1 = accuracy(output.data, target)[0]
+            losses.update(loss.item(), input.size(0))
+            top1.update(prec1.item(), input.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                print('Test: [{0}/{1}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
+                          i, len(val_loader), batch_time=batch_time, loss=losses,
+                          top1=top1))
+
+    print(' * Prec@1 {top1.avg:.3f}'
+          .format(top1=top1))
         # Add data to TensorBoard
         writer.add_scalar('Test/Loss', losses.avg, epoch * len(val_loader) + i)
         writer.add_scalar('Test/Accuracy', top1.avg, epoch * len(val_loader) + i)
@@ -204,14 +281,41 @@ def validate(val_loader, model, criterion):
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     # ...
+    torch.save(state, filename)
 
 
 class AverageMeter(object):
     # ...
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
 
 def accuracy(output, target, topk=(1,)):
     # ...
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
 
 
 if __name__ == '__main__':
